d542b43dc36bff09f74fa847adf2508771d37798
Mikhail Borozdin <mikhail.borozdin@cloudreach.co.uk>
2012-03-01 18:06:46 +0000

    Uploading to Git

---
+++ Application/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MyApplication
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void saveButton_Click(object sender, RoutedEventArgs e)
+        {
+            bool isCompanyEmpty = false;
+
+            if (string.IsNullOrWhiteSpace(this.companyTextBox.Text))
+            {
+                isCompanyEmpty = true;
+
+                this.companyError.Text = "Company name is empty";
+            }
+            else
+            {
+                this.companyError.Text = string.Empty;
+            }
+
+            if (!isCompanyEmpty && !string.IsNullOrWhiteSpace(this.emailTextBox.Text))
+            {
+                this.customersListView.Items.Add(this.companyTextBox.Text);
+            }
+        }
+    }
+}
