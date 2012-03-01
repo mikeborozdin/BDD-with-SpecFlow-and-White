c688c03696b71ad1cec3a9f8c241b14f6ea7e1b1
Mikhail Borozdin <mikhail.borozdin@cloudreach.co.uk>
2012-03-01 18:07:15 +0000

    Uploading to Git

---
+++ Tests/SavingCustomerInformation.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using TechTalk.SpecFlow;
+using White.Core;
+using White.Core.UIItems.WindowItems;
+using White.Core.UIItems;
+using White.Core.UIItems.ListBoxItems;
+using White.Core.UIItems.Finders;
+
+namespace Tests
+{
+    [Binding]
+    public class SavingCustomerInformation
+    {
+        private static Application applicationUnderTest;
+        private Window newCustomerWindow;
+        private const string COMPANY = "Some Company";
+
+        [BeforeFeature]
+        public static void LaunchApplication()
+        {
+            SavingCustomerInformation.applicationUnderTest = Application.Launch(@"..\..\..\Application\bin\Debug\Application.exe");
+        }
+
+        [Given("I am in the New Customer window")]
+        public void GivenNewCustomerWindow()
+        {
+            this.newCustomerWindow = SavingCustomerInformation.applicationUnderTest.GetWindow("New Customer");
+        }
+
+        [When("I enter a company name in the company name field")]
+        public void WhenEnterCompanyName()
+        {
+            TextBox companyTextBox = this.newCustomerWindow.Get<TextBox>("companyTextBox");
+            companyTextBox.Enter(COMPANY);
+        }
+
+        [When("I enter a contact e-mail address in the e-mail field")]
+        public void WhenEnterEmail()
+        {
+            TextBox emailTextBox = this.newCustomerWindow.Get<TextBox>("emailTextBox");
+            emailTextBox.Enter("some@email.com");
+        }
+
+        [When("I click on the Save button")]
+        public void WhenClickSaveButton()
+        {
+            Button saveButton = this.newCustomerWindow.Get<Button>("saveButton");
+            saveButton.Click();
+        }
+
+        [Then("I should see a newly added customer in the list of customers filed by their company name")]
+        public void ThenNewCustomerInList()
+        {
+            ListBox customersList = this.newCustomerWindow.Get<ListBox>("customersListView");
+
+            Assert.NotNull(customersList.Items.Find(x => x.Text == COMPANY));
+        }
+
+        [When("I leave the company name field blank")]
+        public void WhenLeaveBlankCompanyName()
+        {
+            TextBox companyTextBox = this.newCustomerWindow.Get<TextBox>("companyTextBox");
+            companyTextBox.Enter(string.Empty);
+        }
+
+        [Then("I should be presented with an error saying so")]
+        public void ThenError()
+        {
+            Assert.NotNull(this.newCustomerWindow.Get(SearchCriteria.ByText("Company name is empty")));
+        }
+
+        [AfterFeature]
+        public static void KillApplication()
+        {
+            SavingCustomerInformation.applicationUnderTest.Kill();
+        }
+    }
+}
---
+++ Tests/SavingCustomerInformation.feature
@@ -0,0 +1,17 @@
+﻿Feature: Saving Customer Information
+As a sales manager
+I want to save a customer’s details
+So that I can contact them later
+
+Scenario: HappyPath
+Given I am in the New Customer window
+When I enter a company name in the company name field
+When I enter a contact e-mail address in the e-mail field
+When I click on the Save button
+Then I should see a newly added customer in the list of customers filed by their company name
+
+Scenario: Blank Company Name
+Given I am in the New Customer window
+When I leave the company name field blank
+When I click on the Save button
+Then I should be presented with an error saying so
